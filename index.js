83ce6769b72142b992097b42f077d93948df24f8
Sam Niebergall <sam@samniebergall.com>
2023-09-23 15:56:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-let annDate = params.date ?? '2022-09-22'; // "some_value"
+let annDate = params.date ?? '2023-09-22'; // "some_value"
 
 
 const name1 = params.name1 ?? "Jackson";
